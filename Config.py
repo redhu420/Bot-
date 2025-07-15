39a2b51e8d771908c8ef59134ca366342a7ede3f
redhu420 <princeredhu72@gmail.com>
2025-07-15 14:10:13 +0530

    Create Config.py

---
+++ Config.py
@@ -0,0 +1,10 @@
+import os
+
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+STRING_SESSION = os.getenv("STRING_SESSION")
+OWNER_ID = int(os.getenv("OWNER_ID"))
+
+SPOTIFY_CLIENT_ID = os.getenv("SPOTIFY_CLIENT_ID")
+SPOTIFY_CLIENT_SECRET = os.getenv("SPOTIFY_CLIENT_SECRET")
